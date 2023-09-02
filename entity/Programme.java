6914e917ead16e073141e628e13cf05e701139fc
Fulim13 <87457764+Fulim13@users.noreply.github.com>
2023-09-02 11:22:06 +0800

    Change Programme entity

--- entity/Programme.java
+++ entity/Programme.java
@@ -1,50 +1,31 @@
 package entity;
 
+import java.util.ArrayList;
+
 public class Programme implements Comparable<Programme> {
     private int code;
     private String name;
+    private String type;
     private int duration;
     private String faculty;
 
-    private TutorialGroup[] tutorialGroup;
+    private ArrayList<TutorialGroup> tutorialGroup;
 
-    public Programme(int code, String name, int duration, String faculty) {
+    public Programme(int code, String name, String type, int duration, String faculty) {
         this.code = code;
         this.name = name;
+        this.type = type;
         this.duration = duration;
         this.faculty = faculty;
     }
 
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
+    public Programme(int code, String name, String type, int duration, String faculty, ArrayList<TutorialGroup> tutorialGroup) {
         this.code = code;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public void setDuration(int duration) {
+        this.type = type;
         this.duration = duration;
-    }
-
-    public String getFaculty() {
-        return faculty;
-    }
-
-    public void setFaculty(String faculty) {
         this.faculty = faculty;
+        this.tutorialGroup = tutorialGroup;
     }
 
     @Override
@@ -53,22 +34,32 @@ public class Programme implements Comparable<Programme> {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Programme programme = (Programme) o;
+        return code == programme.code;
+    }
+
+    @Override
     public String toString() {
         return "Programme{" +
-                "code='" + code + '\'' +
+                "code=" + code +
                 ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
                 ", duration=" + duration +
                 ", faculty='" + faculty + '\'' +
+                ", tutorialGroup=" + tutorialGroup +
                 '}';
     }
 
     @Override
     public int compareTo(Programme o) {
-        if(this.code > o.code){
+        if (this.code > o.code) {
             return 1;
-        }else if(this.code < o.code){
+        } else if (this.code < o.code) {
             return -1;
-        }else{
+        } else {
             return 0;
         }
     }
