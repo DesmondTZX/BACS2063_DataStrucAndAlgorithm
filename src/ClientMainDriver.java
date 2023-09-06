7268ffc14d1a2491ee1be2ba7a62cc0d2045090b
DesmondTZX <desmondtzx@outlook.com>
2023-09-06 10:57:24 +0800

    added main driver program

---
+++ src/ClientMainDriver.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+import control.*;
+import static control.TutorGroupManagement.autoGenerate;
+
+/**
+ *
+ * @author Desmond
+ */
+public class ClientMainDriver {
+    Scanner scanner = new Scanner(System.in);
+    
+    public int getSystemChoice() {
+        System.out.println("\nUniversity Programme, Tutorial Group, Tutor Management Systems");
+        System.out.println("1. Programme Management");
+        System.out.println("2. Tutorial Group Management");
+        System.out.println("3. Tutor Management");
+        System.out.println("0. Quit");
+        System.out.print("Enter choice: ");
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println();
+        return choice;
+    }
+    
+    public static void main(String[] args) {
+        ClientMainDriver client = new ClientMainDriver(); // Create an instance of ClientMainDriver
+        int choice = client.getSystemChoice(); // Call getSystemChoice on the instance
+
+        while (choice != 0) {
+            switch (choice) {
+                case 1 -> {
+                    ProgrammeManagement programmeManagement = new ProgrammeManagement();
+                    programmeManagement.start();
+                }
+                case 2 -> {
+                    TutorGroupManagement tutGroupManagement = new TutorGroupManagement();
+                    autoGenerate(tutGroupManagement);
+                    tutGroupManagement.mainMenu();
+                }   
+                case 3 -> {
+                    TutorManagement tutorManagement = new TutorManagement();
+                    tutorManagement.runTutorManagement();
+                }
+                default -> System.out.println("\nInvalid choice");
+            }
+
+            choice = client.getSystemChoice(); // Update the choice for the next iteration
+        }
+
+        System.out.println("\nExiting program.");
+    }
+}
